308c6e07531e82fe289e568d05bd6b4f1e2f14e5
derfliwbej <jwdpanganiban@gmail.com>
2023-07-13 18:51:37 +0800

    Feat: Add URL validation

--- src/components/landing/Hero.js
+++ src/components/landing/Hero.js
@@ -3,6 +3,8 @@ import { Anton } from "next/font/google";
 import { useState } from "react";
 import Image from "next/image";
 
+import Alert from "@/components/ui/Alert";
+
 const anton = Anton({ 
   subsets: ['latin'],
   weight: '400'
@@ -11,6 +13,8 @@ const anton = Anton({
 export default function Hero() {
     const [urlInput, setUrlInput] = useState('');
     const [btnDisabled, setBtnDisabled] = useState(true);
+    const [showAlert, setShowAlert] = useState(false);
+    const [alert, setAlert] = useState({ type: 'prompt', message: '' });
 
     const handleInputChange = (e) => {
         const input = e.target.value;
@@ -22,9 +26,27 @@ export default function Hero() {
         } else setBtnDisabled(false);
     }
 
+    const createURL = (url) => {
+        try {
+            return new URL(url);
+        } catch(e) {
+            return false;
+        }
+    };
+
     const handleAuthorize = (e) => {
         e.preventDefault();
 
+        const urlObject = createURL(urlInput);
+        const hash = urlObject['hash'];
+        const parsedHash = new URLSearchParams(hash?.substring(1));
+        const access_token = parsedHash?.get('access_token');
+
+        if(!urlObject || !hash || !parsedHash || !access_token) {
+            setAlert({ type: "danger", message: "Invalid URL"});
+            setShowAlert(true);
+        }
+        
         setUrlInput('');
         setBtnDisabled(true);
     };
@@ -47,6 +69,7 @@ export default function Hero() {
                 <Image src="/hero-img.jpg" fill alt="Hero" objectFit="cover" quality={100} />
             </div>
             <div className="absolute w-full h-screen -z-10 bg-gradient-to-t from-black to-transparent"></div>
+            <Alert message={alert.message} type={alert.type} open={showAlert} setOpen={setShowAlert}/>
         </section>
     );
 }

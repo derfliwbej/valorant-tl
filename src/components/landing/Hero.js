f9d0b9c12af167f428f17fb0bf19b2de2a230c81
derfliwbej <jwdpanganiban@gmail.com>
2023-07-13 20:42:38 +0800

    Feat: Add auth logic

--- src/components/landing/Hero.js
+++ src/components/landing/Hero.js
@@ -2,8 +2,10 @@
 import { Anton } from "next/font/google";
 import { useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import Alert from "@/components/ui/Alert";
+import TopLoading from "@/components/ui/TopLoading";
 
 const anton = Anton({ 
   subsets: ['latin'],
@@ -11,10 +13,12 @@ const anton = Anton({
 });
 
 export default function Hero() {
+    const router = useRouter();
     const [urlInput, setUrlInput] = useState('');
     const [btnDisabled, setBtnDisabled] = useState(true);
     const [showAlert, setShowAlert] = useState(false);
     const [alert, setAlert] = useState({ type: 'prompt', message: '' });
+    const [loading, setLoading] = useState(false);
 
     const handleInputChange = (e) => {
         const input = e.target.value;
@@ -36,6 +40,8 @@ export default function Hero() {
 
     const handleAuthorize = (e) => {
         e.preventDefault();
+        setShowAlert(false);
+        setAlert({ type: '', message: '' });
 
         const urlObject = createURL(urlInput);
         const hash = urlObject['hash'];
@@ -45,8 +51,42 @@ export default function Hero() {
         if(!urlObject || !hash || !parsedHash || !access_token) {
             setAlert({ type: "danger", message: "Invalid URL"});
             setShowAlert(true);
+
+            setUrlInput('');
+            setBtnDisabled(true);
+
+            return;
         }
-        
+
+        const authorize = async (access_token) => {
+            setLoading(true);
+            try {
+                const res = await fetch('http://localhost:3000/api/auth', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    credentials: 'same-origin',
+                    body: JSON.stringify({ access_token })
+                });
+
+                const { message } = await res.json();
+
+                if (res.status >= 400) {
+                    throw new Error(message);
+                }
+
+                router.push('/store');
+            } catch(e) {
+                setAlert({ type: "danger", message: e.message });
+                setShowAlert(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        authorize(access_token);
+
         setUrlInput('');
         setBtnDisabled(true);
     };
@@ -57,8 +97,8 @@ export default function Hero() {
                 <h2 className={`${anton.className} text-7xl font-bold text-red-500 my-5`}>ValorantTL</h2>
                 <p className="text-white text-xl my-5">Nulla deserunt anim non non aliqua ut ad. Sint quis ad do nisi ad ea eu enim minim aliquip excepteur sint veniam. Id irure est sunt do commodo commodo dolore laborum ut ex tempor.</p>
                 <form onSubmit={handleAuthorize} className="flex">
-                    <input type="text" className="w-full py-2 px-3" placeholder="Enter URL here" onChange={handleInputChange} value={urlInput} />
-                    <button disabled={btnDisabled} 
+                    <input disabled={loading} type="text" className="w-full py-2 px-3 text-black" placeholder="Enter URL here" onChange={handleInputChange} value={urlInput} />
+                    <button disabled={btnDisabled || loading} 
                             className={"px-3 font-bold text-white " + (
                                        btnDisabled ? "bg-gray-500" : "bg-red-500 hover:brightness-95"
                                        )} 
@@ -70,6 +110,7 @@ export default function Hero() {
             </div>
             <div className="absolute w-full h-screen -z-10 bg-gradient-to-t from-black to-transparent"></div>
             <Alert message={alert.message} type={alert.type} open={showAlert} setOpen={setShowAlert}/>
+            {loading && <TopLoading />}
         </section>
     );
 }

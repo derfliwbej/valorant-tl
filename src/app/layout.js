d544b2b8bf2b7e8c2b6e4cab3b76042806ecf8a4
derfliwbej <jwdpanganiban@gmail.com>
2023-07-13 20:41:27 +0800

    Style: Make white as default foreground color

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} bg-slate-950`}>{children}</body>
+      <body className={`${roboto.className} bg-slate-950 text-white`}>{children}</body>
     </html>
   )
 }

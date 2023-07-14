ba1a26c4012c7ec00ddbb4fb1b56a606e0c34abd
derfliwbej <jwdpanganiban@gmail.com>
2023-07-14 20:48:13 +0800

    Feat: Create provider for user

---
+++ src/components/providers/AuthProvider.js
@@ -0,0 +1,44 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+import { useRouter } from "next/navigation";
+import useSWR from "swr";
+
+export const AuthContext = createContext({
+    user: null,
+    error: null,
+    isLoading: true,
+});
+
+export default function AuthProvider({ children }) {
+    const router = useRouter();
+
+    const getUser = async () => {
+        const res = await fetch('http://localhost:3000/api/user');
+        const body = await res.json();
+
+        if(res.status >= 400) {
+            return null;
+        } else {
+            return body;
+        }
+    };
+
+    const { data: user, error, isLoading } = useSWR("/api/user", getUser);
+
+    const exposed = {
+        user,
+        error,
+        isLoading
+    };
+
+    return <AuthContext.Provider value={exposed}>{children}</AuthContext.Provider>
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error("useAuth must be used inside AuthProvider");
+    } else {
+        return context;
+    }
+};

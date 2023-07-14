b674005e77aa603ca5785d57af68d8eaf938fc45
derfliwbej <jwdpanganiban@gmail.com>
2023-07-14 20:49:28 +0800

    Feat: Create Avatar component

---
+++ src/components/ui/Avatar.js
@@ -0,0 +1,28 @@
+import useSWR from "swr";
+import { useAuth } from "@/components/providers/AuthProvider";
+import Image from "next/image";
+
+export default function Avatar({ size }) {
+    const { user } = useAuth();
+
+    const avatarFetcher = async (id) => {
+        const res = await fetch(`https://valorant-api.com/v1/playercards/${id}`);
+
+        if (res.status >= 400) {
+            return null;
+        }
+
+        const body = await res.json();
+        const { displayIcon } = body.data;
+
+        return displayIcon;
+    };
+
+    const { data: avatarURL, isLoading: avatarLoading, error: avatarError } = useSWR(user?.card_id, avatarFetcher);
+
+    if (!avatarLoading && avatarURL) {
+        return (
+            <Image className="rounded-full" src={avatarURL} alt="Player Card" height={size} width={size} />
+        );
+    } else return <></>
+}

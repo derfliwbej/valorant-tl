f77e8b7d7ea71374de20ea237c8f28cb99778365
derfliwbej <jwdpanganiban@gmail.com>
2023-07-14 20:50:25 +0800

    Chore: Add animation for drawer

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        'drawer-open': 'drawer-open 0.5s ease-in-out',
+        'show-overlay': 'show-overlay 0.5s ease-in-out'
+      },
+      keyframes: {
+        'drawer-open': {
+          '0%': {
+            left: '-100%'
+          },
+          '50%': {
+            left: '-50%'
+          },
+          '100%': {
+            left: '0px',
+            opacity: 0.8
+          }
+        },
+        'show-overlay': {
+          '0%': { opacity: 0 },
+          '50%': { opacity: 0 },
+          '100%': { opacity: 0.8 },
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':

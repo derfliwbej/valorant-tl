ca6970791ccb99596644cec306f6adea31100437
derfliwbej <jwdpanganiban@gmail.com>
2023-07-16 15:41:32 +0800

    Feat: useSWR cache optimizations

--- src/components/store/Storefront.js
+++ src/components/store/Storefront.js
@@ -1,3 +1,4 @@
+'use client';
 import StoreItem from "@/components/store/StoreItem";
 import StoreItemSkeleton from "@/components/store/StoreItemSkeleton";
 import useSWR from "swr";
@@ -7,7 +8,7 @@ import { useAuth } from "@/components/providers/AuthProvider";
 export default function Storefront() {
     const { user } = useAuth();
 
-    const { data: items, isLoading, error } = useSWR(user?.puuid, async () => {
+    const { data: items, isLoading, error } = useSWR(`/api/store/${user?.puuid}`, async () => {
         const res = await fetchUtil("/api/store");
 
         if (res.status >= 400) return null;
@@ -19,8 +20,7 @@ export default function Storefront() {
 
     return (
         <div className="flex flex-wrap justify-evenly gap-5">
-            { error ? 'Error fetching items' : (
-            !isLoading && items ? items.map( (item, i) => {
+            { !error && !isLoading && items ? items.map( (item, i) => {
                 return <StoreItem key={i} name={item.name} cost={item.cost} displayIcon={item.displayIcon} />
             }) : (
                 <>
@@ -29,7 +29,7 @@ export default function Storefront() {
                     <StoreItemSkeleton />
                     <StoreItemSkeleton />
                 </>
-            ))}
+            )}
         </div>
     );
 }

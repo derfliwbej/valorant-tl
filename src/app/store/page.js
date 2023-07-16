7d8056d4579113ceb3b8c686ad1b2b9175372193
derfliwbej <jwdpanganiban@gmail.com>
2023-07-16 12:25:31 +0800

    Refactor: Wrap store items into Storefront component

--- src/app/store/page.js
+++ src/app/store/page.js
@@ -2,10 +2,7 @@
 
 import { Anton } from "next/font/google";
 import UserContextWrapper from "@/components/layouts/UserContextWrapper";
-import StoreItem from "@/components/store/StoreItem";
-import StoreItemSkeleton from "@/components/store/StoreItemSkeleton";
-import useSWR from "swr";
-import fetchUtil from "@/utils/fetchUtil";
+import Storefront from "@/components/store/Storefront";
 
 const anton = Anton({
     subsets: ['latin'],
@@ -13,32 +10,11 @@ const anton = Anton({
 });
 
 export default function Store() {
-    const { data: items, isLoading, error } = useSWR("api/store", async (url) => {
-        const res = await fetchUtil(`/${url}`);
-
-        if (res.status >= 400) return null;
-
-        const { items } = await res.json();
-
-        return items;
-    });
 
     return (
         <UserContextWrapper>
             <h1 className={`${anton.className} text-3xl text-red-500 mb-5`}>Storefront</h1>
-            {error && 'Error fetching items'}
-            <div className="flex flex-wrap justify-evenly gap-5">
-                {!isLoading && items ? items.map( (item, i) => {
-                    return <StoreItem key={i} name={item.name} cost={item.cost} displayIcon={item.displayIcon} />
-                }) : (
-                    <>
-                        <StoreItemSkeleton />
-                        <StoreItemSkeleton />
-                        <StoreItemSkeleton />
-                        <StoreItemSkeleton />
-                    </>
-                )}
-            </div>
+            <Storefront />
         </UserContextWrapper>
     );
 }
---
+++ src/components/store/Storefront.js
@@ -0,0 +1,35 @@
+import StoreItem from "@/components/store/StoreItem";
+import StoreItemSkeleton from "@/components/store/StoreItemSkeleton";
+import useSWR from "swr";
+import fetchUtil from "@/utils/fetchUtil";
+import { useAuth } from "@/components/providers/AuthProvider";
+
+export default function Storefront() {
+    const { user } = useAuth();
+
+    const { data: items, isLoading, error } = useSWR(user?.puuid, async () => {
+        const res = await fetchUtil("/api/store");
+
+        if (res.status >= 400) return null;
+
+        const { items } = await res.json();
+
+        return items;
+    });
+
+    return (
+        <div className="flex flex-wrap justify-evenly gap-5">
+            { error ? 'Error fetching items' : (
+            !isLoading && items ? items.map( (item, i) => {
+                return <StoreItem key={i} name={item.name} cost={item.cost} displayIcon={item.displayIcon} />
+            }) : (
+                <>
+                    <StoreItemSkeleton />
+                    <StoreItemSkeleton />
+                    <StoreItemSkeleton />
+                    <StoreItemSkeleton />
+                </>
+            ))}
+        </div>
+    );
+}
